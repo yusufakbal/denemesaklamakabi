52895e075d297098c0cf3c7674b242aa6f51bf7a
Yusuf Akbal <71309216+yusufakbal@users.noreply.github.com>
2022-10-16 16:38:09 +0300

    Add files via upload

---
+++ insert_data_trying.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+$servername = "saklamakabi.mysql.database.azure.com";
+$username = "saklamakabi";
+$password = "Project-3434";
+$database_name = "saklamakabi";
+
+// Create MySQL connection fom PHP to MySQL server
+$connection = new mysqli($servername, $username, $password, $database_name);
+// Check connection
+if ($connection->connect_error) {
+   die("MySQL connection failed: " . $connection->connect_error);
+}
+
+$sql = "INSERT INTO saklamakabi.mesafe (kartid) VALUES (2)";
+
+if ($connection->query($sql) === TRUE) {
+   echo "New record created successfully";
+} else {
+   echo "Error: " . $sql . " => " . $connection->error;
+}
+
+$connection->close();
